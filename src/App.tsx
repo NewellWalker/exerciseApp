11428ffa06b9d2061ceb1ab2c5743d81a2d82b84
NewellWalker <Newell.Walker.1@gmail.com>
2023-05-16 21:00:00 -0400

    imported and added sign-up route

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import About from './components/About';
 import CurrentWorkout from './components/CurrentWorkout';
 import MuscleGroups from './components/MuscleGroups';
 import Learn from './components/Learn';
+import SignUp from './components/SignUp';
 import './App.css';
 
 import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
@@ -11,6 +12,7 @@ import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } 
 const router = createBrowserRouter(createRoutesFromElements(
   <Route path="/" element={ <Root/> }>
     <Route path="home" element={ <Home /> } />
+    <Route path="sign-up" element={ <SignUp /> }/>
     <Route path="about" element={ <About /> } />
     <Route path="current-workout" element={ <CurrentWorkout /> } />
     <Route path="muscle-groups" element={ <MuscleGroups /> } />
