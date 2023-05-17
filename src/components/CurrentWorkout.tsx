40cf05e0704ef71052bac206af39be3eb4025347
NewellWalker <Newell.Walker.1@gmail.com>
2023-05-16 20:57:19 -0400

    added notes for future work

--- src/components/CurrentWorkout.tsx
+++ src/components/CurrentWorkout.tsx
@@ -1,4 +1,11 @@
+// generate workout given selection boxes
+// could potentially use AI in later versions, at first just use rng and class of movements (compound, accessory, steady state cardio, intervals, circuit)
+// "Generate random workout!" button
+// could have place that workout are shared and the most popular ones can be copied over (like social media / bodybuilding.com) but that is much further down the road
+
 export default function CurrentWorkout() {
+
+
     return (
         <h1>workout</h1>
     );
