d8a9acc355c6e3895915d552c928d58f919df517
NewellWalker <Newell.Walker.1@gmail.com>
2023-05-16 20:54:36 -0400

    updated footer to have content and input fields

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,12 @@
 export default function Footer() {
     return (
-        <h3>Footer</h3>
+        <>
+            <h2>Have an exercise we are missing? Let us know!</h2>
+            <label htmlFor="exerciseInput">What is it? </label>
+            <input type="text" id="exerciseInput" />
+            <br />
+            <label htmlFor="email">How can we reach out make sure get it right? </label>
+            <input type="text" id="email" />
+        </>
     );
 }
