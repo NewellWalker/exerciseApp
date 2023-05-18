6ac80b237eec65cf09749769fc9007b10cc48db0
NewellWalker <Newell.Walker.1@gmail.com>
2023-05-17 20:02:47 -0400

    added input fields and some validation

--- src/components/SignUp.tsx
+++ src/components/SignUp.tsx
@@ -1,6 +1,72 @@
+
+
 export default function SignUp() {
+
+    const onSubmit = (e:any) => {
+        e.preventDefault();
+    };
+
     // sign up form
     return (
-        <h1>sign-up form placeholder</h1>
+        <form 
+            onSubmit={onSubmit}
+            noValidate
+        >
+            <label htmlFor="firstName">First name: </label>
+            <input
+                type="text"
+                name="firstName"
+                id="firstName"
+                placeholder="What's your first name?"
+                required
+            />
+            <br />
+            <div className="goalsChecklist">
+                <label htmlFor="goals">What are you goals? To...</label>
+                <br />
+                <label htmlFor="loseWeight">Lose weight </label>
+                <input type="checkbox" name="loseWeight" id="loseWeight" />
+                <br />
+                <label htmlFor="gainMuscle">Gain muscle</label>
+                <input type="checkbox" name="gainMuscle" id="gainMuscle" />
+                <br />
+                <label htmlFor="betterShape">Get in better shape </label>
+                <input type="checkbox" name="betterShape" id="betterShape" />
+                <br />
+                <label htmlFor="getStronger">Get Stronger </label>
+                <input type="checkbox" name="getStronger" id="getStronger" />
+                <br />
+                <label htmlFor="other">Other: </label>
+                <input type="checkbox" name="other" id="other" />
+                <input type="text" name="other" id="other" placeholder="What's your goal?" />
+                <br />
+            </div>
+            <label htmlFor="lastName">Last name: </label>
+            <input
+                type="text"
+                name="lastName"
+                id="lastName"
+                placeholder="What's your last name?"
+                required
+            />
+            <br />
+            <label htmlFor="email">Email: </label>
+            <input 
+                type="email"
+                name="email"
+                id="email"
+                placeholder="What's your email?"
+                required
+            />
+            <br />
+            <label htmlFor="phone">Phone number (with area code): </label>
+            <input
+                type="text" 
+                name="phone" 
+                placeholder="What's your phone number? (optional)"
+                size={29}
+            />
+            <br />
+        </form>
     );
 }
