116bc4b9a25f36a3a193c49af7a9ab8a9c306acb
NewellWalker <Newell.Walker.1@gmail.com>
2023-05-19 23:29:56 -0400

    trying to iterate over obj to not type out card

---
+++ src/components/types.ts
@@ -0,0 +1,21 @@
+// // https://stackoverflow.com/questions/45670705/iterate-over-interface-properties-in-typescript
+
+// // top level muscle object
+// export const MuscleObj {
+//     // chest, back, etc.
+//     muscleGroup: "";
+
+//     // upper pec, lower pec, etc.
+//     subGroup: "";
+
+//     movements: ;
+//     // stretch, contraction
+//     muscleAction: "";
+//     // image: ;
+//   }
+
+// type MuscleObj = typeof MuscleObj;
+// const card: MuscleObj<Object> = Object.keys(MuscleObj.map(key))
+// // interface for secondary movers for muscle interface
+
+// // interface for tertiary tags (plyo, explosion, etc. ) for muscle interface
