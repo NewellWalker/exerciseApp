7c6b6c9c7ed4b810643cad0a23ddec2c30c5a350
NewellWalker <Newell.Walker.1@gmail.com>
2023-05-20 10:40:29 -0400

    commented out form validation

--- src/components/types.ts
+++ src/components/types.ts
@@ -1,21 +1,32 @@
-// // https://stackoverflow.com/questions/45670705/iterate-over-interface-properties-in-typescript
 
-// // top level muscle object
-// export const MuscleObj {
+
+// https://stackoverflow.com/questions/45670705/iterate-over-interface-properties-in-typescript
+
+// top level muscle object
+// export const MuscleObj = {
 //     // chest, back, etc.
-//     muscleGroup: "";
+//     muscleGroup: "",
 
 //     // upper pec, lower pec, etc.
-//     subGroup: "";
+//     subGroup: "",
 
-//     movements: ;
+//     movements: ,
 //     // stretch, contraction
-//     muscleAction: "";
+//     muscleAction: "",
 //     // image: ;
 //   }
 
 // type MuscleObj = typeof MuscleObj;
 // const card: MuscleObj<Object> = Object.keys(MuscleObj.map(key))
-// // interface for secondary movers for muscle interface
+// interface for secondary movers for muscle interface
 
-// // interface for tertiary tags (plyo, explosion, etc. ) for muscle interface
+// interface for tertiary tags (plyo, explosion, etc. ) for muscle interface
+
+// form validation template
+// type UserSubmitForm = {
+//     firstName: string,
+//     goalsChecklist: boolean, // will set to false by default and if no boxes are checked, true if any are checked
+//     lastName: string,
+//     email: string,
+//     phone?: Number
+// }
