48fc7a5dc864ccfda4fc9f144e23f2f5ce474e83
NewellWalker <Newell.Walker.1@gmail.com>
2023-05-20 10:44:48 -0400

    commented out unused items to clear errors

--- src/components/MuscleGroups/MuscleGroupCards/MuscleGroupCard.tsx
+++ src/components/MuscleGroups/MuscleGroupCards/MuscleGroupCard.tsx
@@ -3,37 +3,16 @@
 
 
 import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
+// import CardActions from '@mui/material/CardActions';
+// import CardContent from '@mui/material/CardContent';
+// import CardMedia from '@mui/material/CardMedia';
+// import Button from '@mui/material/Button';
+// import Typography from '@mui/material/Typography';
 
 export default function MuscleGroupCard() {
     return (
-        <Card variant="outlined" sx={{maxWidth: 'md'}}>
-            <CardMedia
-                sx={{
-                    minHeight:140,
-                    
-                }}
-                image='src/assets/AF.jfif'
-            />
-            <CardContent>
-                <Typography variant="h5">
-                    Test
-                </Typography>
-            </CardContent>
-            <CardActions>
-                <Button
-                    size="small"
-                    sx={{
-                        backgroundColor: "lightGray",
-                    }}
-                >
-                    
-                </Button>
-            </CardActions>
+        <Card variant="outlined" >
+           card
         </Card>
     )
 }
