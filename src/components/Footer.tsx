6b472530e387a0bc8d2c4809532f9252c939dbb1
NewellWalker <Newell.Walker.1@gmail.com>
2023-05-22 22:30:46 -0400

    removed content to start blank

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,35 +1,8 @@
 export default function Footer() {
 
-    const onSubmit = (e:any) => {
-        e.preventDefault();
-    }
 
     return (
         <>
-            <h3>footer break</h3>
-            <h2>Have an exercise we are missing? Let us know!</h2>
-            <form
-                action="post"
-                onSubmit={onSubmit}
-                noValidate
-            >
-                <label htmlFor="exerciseInput">Exercise </label>
-                <input 
-                    type="text"
-                    id="exerciseInput"
-                    placeholder="What's the name and description"
-                    size={25}
-                    required
-                />
-                <br />
-                <label htmlFor="email">How can we reach out make sure get it right? </label>
-                <input
-                    type="text"
-                    id="email"
-                    placeholder="Email address"
-                />
-                <input type="button" value="Submit" />
-            </form>
         </>
     );
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,35 +1,9 @@
-import * as React from 'react';
-import { NavLink } from "react-router-dom";
+// import { NavLink } from "react-router-dom";
 
 export default function Header() {
 
-    // add breadcrumbs to make navigating easier when looking at muscle groups/exercise types
-
-    const [ search, setSearch ] = React.useState('')
-
-    const handleClick = (e: any) => {
-        e.target.value;
-    }
-
-    const onChange = (e: any) => {
-        setSearch(e.target.value);
-        search;
-    }
-
     return (
         <>
-            <NavLink to='/home'>Home</NavLink>
-            <NavLink to='/about'>About</NavLink>
-            <NavLink to='/current-workout'>Create Your Workout</NavLink>
-            <NavLink to='/muscle-groups'>Muscles</NavLink>
-            <NavLink to='/learn'>Learn</NavLink>
-            < input
-                type="text"
-                onChange={onChange}
-                placeholder='Search'
-            />
-            <img src="src\assets\search.png" alt="" onSubmit={handleClick} />
-            <h3>header break</h3>
         </>
     );
 }
