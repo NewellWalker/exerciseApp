11b0b57eaf1742b07612915f8f124b9b08addc56
NewellWalker <Newell.Walker.1@gmail.com>
2023-06-03 18:37:44 -0400

    created a page to list all exercises

---
+++ src/components/Exercises.tsx
@@ -0,0 +1,17 @@
+
+export default function Exercises() {
+
+    return (
+        <div>
+            <h2>Resistance Movements</h2>
+            <h1>make these nested lists</h1>
+            <div
+                className="resistanceMovement"
+            >
+                <h3 className="upperChest" >Incline fly</h3>
+                <h3 className="upperChest" >Incline barbell bench press</h3>
+                <h3 className="upperChest" >Incline dumbell bench press</h3>
+            </div>
+        </div>
+    );
+}
